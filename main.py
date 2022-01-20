75460d1fdb51b7071c7e0d4eb68ebf0fd04fa291
Johnny HomoMan <98095546+SanockieHotuwy@users.noreply.github.com>
2022-01-20 19:05:24 +0000

    1.6 Bugged

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ wygrana = False
 lista = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
 lista2 = ["  ","  ","  ","  ","  ","  ","  ","  ","  ",]
 def pokaz():
-  clearConsole()
+  #clearConsole()
   print("    1    2    3")
   print(" ")
   print("A   "+lista2[0]+" | "+lista2[1]+" | "+lista2[2])
@@ -23,58 +23,146 @@ while wygrana != True:
   print("")
   nrPola = input("Podaj Nr Pola: ")
   if nrPola == "A1":
-    lista2[0] = "O "
+    if lista[0] == "A1":
+      lista2[0] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("COs nie ten teges")
   if nrPola == "A2":
-    lista2[1] = "O "
+    if lista[1] == "A2":
+      lista2[1] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("Cos nie ten teges")
   if nrPola == "A3":
-    lista2[2] = "O "
+    if lista[2] == "A3":
+      lista2[2] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("COs nie ten teges")
   if nrPola == "B1":
-    lista2[3] = "O "
+    if lista[3] == "B1":
+      lista2[3] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("COs nie ten teges")
   if nrPola == "B2":
-    lista2[4] = "O "
+    if lista[4] == "B2":
+      lista2[4] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("COs nie ten teges")
   if nrPola == "B3":
-    lista2[5] = "O "
+    if lista[5] == "B3":
+      lista2[5] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("COs nie ten teges")
   if nrPola == "C1":
-    lista2[6] = "O "
+    if lista[6] == "C1":
+      lista2[6] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("COs nie ten teges")
   if nrPola == "C2":
-    lista2[7] = "O "
+    if lista[7] == "C2":
+      lista2[7] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("COs nie ten teges")
   if nrPola == "C3":
-    lista2[8] = "O "
-  lista = np.array(lista)
-  lista = np.where(lista == nrPola, 'O ', lista)
+    if lista[8] == "C3":
+      lista2[8] = "O "
+      lista = np.array(lista)
+      lista = np.where(lista == nrPola, 'O ', lista)
+    else:
+      print("COs nie ten teges")
+ 
   pokaz()
   if lista[1] == lista[2] == lista[0] or lista[3] == lista[4] == lista[5] or lista[6] == lista[7] == lista[8] or lista[0] == lista[3] == lista[6] or lista[1] == lista[4] == lista[7]or lista[2] == lista[5] == lista[8] or lista[0] == lista[4] == lista[8] or lista[2] == lista[4] == lista[6]:
     print("")
     print("Wygrywa O")
     wygrana = True
-  if wygrana != True:
-    print("")
-    nrPola = input("Podaj Nr Pola: ")
-  if nrPola == "A1":
-    lista2[0] = "X "
-  if nrPola == "A2":
-    lista2[1] = "X "
-  if nrPola == "A3":
-    lista2[2] = "X "
-  if nrPola == "B1":
-    lista2[3] = "X "
-  if nrPola == "B2":
-    lista2[4] = "X "
-  if nrPola == "B3":
-    lista2[5] = "X "
-  if nrPola == "C1":
-    lista2[6] = "X "
-  if nrPola == "C2":
-    lista2[7] = "X "
-  if nrPola == "C3":
-    lista2[8] = "X "
-    lista = np.array(lista)
-    lista = np.where(lista == nrPola, 'X ', lista)
+    if wygrana != True:
+      print("")
+      nrPola = input("Podaj Nr Pola: ")
+      if nrPola == "A1":
+        if lista[0] == "A1":
+          lista2[0] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("COs nie ten teges")
+      if nrPola == "A2":
+        if lista[1] == "A2":
+          lista2[1] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("Cos nie ten teges")
+      if nrPola == "A3":
+        if lista[2] == "A3":
+          lista2[2] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("COs nie ten teges")
+      if nrPola == "B1":
+        if lista[3] == "B1":
+          lista2[3] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("COs nie ten teges")
+      if nrPola == "B2":
+        if lista[4] == "B2":
+          lista2[4] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("COs nie ten teges")
+      if nrPola == "B3":
+        if lista[5] == "B3":
+          lista2[5] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("COs nie ten teges")
+      if nrPola == "C1":
+        if lista[6] == "C1":
+          lista2[6] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("COs nie ten teges")
+      if nrPola == "C2":
+        if lista[7] == "C2":
+          lista2[7] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("COs nie ten teges")
+      if nrPola == "C3":
+        if lista[8] == "C3":
+          lista2[8] = "X "
+          lista = np.array(lista)
+          lista = np.where(lista == nrPola, 'X ', lista)
+        else:
+          print("COs nie ten teges")
     if lista[1] == lista[2] == lista[0] or lista[3] == lista[4] == lista[5] or lista[6] == lista[7] == lista[8] or lista[0] == lista[3] == lista[6] or lista[1] == lista[4] == lista[7]or lista[2] == lista[5] == lista[8] or lista[0] == lista[4] == lista[8] or lista[2] == lista[4] == lista[6]:
         print("")
         print("Wygrywa X")
         wygrana = True
-  
+o kurewka XD
+dobra jutro to zalatwie bo jest duzo jebania jak leci po 2XD 
+XDDDDDDDDDD
 
-
-
+dobra lecemy

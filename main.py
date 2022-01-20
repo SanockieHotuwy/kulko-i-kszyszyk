b558310fed50d9ee38a960c1ffef6c1bd50b31bb
Johnny HomoMan <98095546+SanockieHotuwy@users.noreply.github.com>
2022-01-20 14:49:45 +0000

    1.3

--- main.py
+++ main.py
@@ -5,30 +5,73 @@ clearConsole = lambda: os.system('cls' if os.name in ('nt', 'dos') else 'clear')
 clearConsole()
 wygrana = False
 lista = ["A1","A2","A3","B1","B2","B3","C1","C2","C3"]
+lista2 = ["  ","  ","  ","  ","  ","  ","  ","  ","  ",]
 def pokaz():
   clearConsole()
-  print(lista[0]+" | "+lista[1]+" | "+lista[2])
+  print(lista2[0]+" | "+lista2[1]+" | "+lista2[2])
   print("—— | —— | ——")
-  print(lista[3]+" | "+lista[4]+" | "+lista[5])
+  print(lista2[3]+" | "+lista2[4]+" | "+lista2[5])
   print("—— | —— | ——")
-  print(lista[6]+" | "+lista[7]+" | "+lista[8])
+  print(lista2[6]+" | "+lista2[7]+" | "+lista2[8])
   
-def wygrana(X, Y):
-  if str(X[0]) == str(X[1]) == str(X[2]):
-    Y = True
-    print("wygrana")  
+
 while wygrana != True:
   pokaz()
   print("")
   nrPola = input("Podaj Nr Pola: ")
+  if nrPola == "A1":
+    lista2[0] = "O "
+  if nrPola == "A2":
+    lista2[1] = "O "
+  if nrPola == "A3":
+    lista2[2] = "O "
+  if nrPola == "B1":
+    lista2[3] = "O "
+  if nrPola == "B2":
+    lista2[4] = "O "
+  if nrPola == "B3":
+    lista2[5] = "O "
+  if nrPola == "C1":
+    lista2[6] = "O "
+  if nrPola == "C2":
+    lista2[7] = "O "
+  if nrPola == "C3":
+    lista2[8] = "O "
   lista = np.array(lista)
   lista = np.where(lista == nrPola, 'O ', lista)
   pokaz()
-  print("")
-  nrPola = input("Podaj Nr Pola: ")
-  lista = np.array(lista)
-  lista = np.where(lista == nrPola, 'X ', lista)
-  wygrana(lista, wygrana)
+  if lista[1] == lista[2] == lista[0] or lista[3] == lista[4] == lista[5] or lista[6] == lista[7] == lista[8] or lista[0] == lista[3] == lista[6] or lista[1] == lista[4] == lista[7]or lista[2] == lista[5] == lista[8] or lista[0] == lista[4] == lista[8] or lista[2] == lista[4] == lista[6]:
+    print("")
+    print("Wygrywa O")
+    wygrana = True
+  if wygrana != True:
+    print("")
+    nrPola = input("Podaj Nr Pola: ")
+  if nrPola == "A1":
+    lista2[0] = "X "
+  if nrPola == "A2":
+    lista2[1] = "X "
+  if nrPola == "A3":
+    lista2[2] = "X "
+  if nrPola == "B1":
+    lista2[3] = "X "
+  if nrPola == "B2":
+    lista2[4] = "X "
+  if nrPola == "B3":
+    lista2[5] = "X "
+  if nrPola == "C1":
+    lista2[6] = "X "
+  if nrPola == "C2":
+    lista2[7] = "X "
+  if nrPola == "C3":
+    lista2[8] = "X "
+    lista = np.array(lista)
+    lista = np.where(lista == nrPola, 'X ', lista)
+    if lista[1] == lista[2] == lista[0] or lista[3] == lista[4] == lista[5] or lista[6] == lista[7] == lista[8] or lista[0] == lista[3] == lista[6] or lista[1] == lista[4] == lista[7]or lista[2] == lista[5] == lista[8] or lista[0] == lista[4] == lista[8] or lista[2] == lista[4] == lista[6]:
+        print("")
+        print("Wygrywa X")
+        wygrana = True
+  
 
 
 
